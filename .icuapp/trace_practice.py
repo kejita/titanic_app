71cb666c1a39eb48c7385f11d997769d5a052c63
kejita <kgrino95.rock@gmail.com>
2025-11-17 00:06:30 +0900

    delete pre-commit

---
+++ .icuapp/trace_practice.py
@@ -0,0 +1,10 @@
+from typing import List, Tuple
+
+#
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+# streamlitは、ページ設定やタイトル設定を行うために使用される、マークアップ言語のようなもの
+# setpagestやtitleなどの関数を提供する
+from datetime import datetime
